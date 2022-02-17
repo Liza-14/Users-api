b5436c41e3166bdb1bcc7bc1dd0bda52e785030a
Liza <Lizabvb57@gmail.com>
2022-02-17 20:18:13 +0200

    add url heroku

--- swagger-config.js
+++ swagger-config.js
@@ -10,6 +10,9 @@ export const options = {
       {
         url: "http://localhost:5000",
       },
+      {
+        url: "https://lit-retreat-93907.herokuapp.com/users",
+      },
     ],
   },
   apis: ["./src/routes/users.js"],
